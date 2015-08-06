bd73e2910633b0f1c6cb01eabee403240f8bd28d
renhai.lrh <renhai.lrh@alipay.com>
2015-08-06 14:00:59 +0800

    重构项目，并添加压缩js和css的功能

--- test/assets/fake.js
+++ test/assets/fake.js
@@ -1,1 +1,3 @@
-console.log('Test');
+(function(window){
+  window.console.log('Test');
+})(window);
--- test/test.js
+++ test/test.js
@@ -26,7 +26,7 @@ function parseHelper(rawHtml, cb) {
 describe('gulp-h5packer', function (){
   it('#Link标签全局资源包替换', function(done) {
 
-    parseHelper('<link rel="stylesheet" href="amui.css" data-replace="amui" />', function (file){
+    parseHelper('<link rel="stylesheet" href="amui.css" data-replace="static-amui" />', function (file){
       assert(file.isBuffer());
       assert.equal(file.contents.toString('utf8'), htmlWrapper('<link rel="stylesheet" href="https://a.alipayobjects.com/amui/native/9.0/amui.css">'));
       done();
@@ -35,7 +35,7 @@ describe('gulp-h5packer', function (){
   });
 
   it('#script标签全局资源包替换', function(done) {
-    parseHelper('<script src="lazyload.min.js" charset="utf-8" data-replace="lazyload"></script>', function (file){
+    parseHelper('<script src="lazyload.min.js" charset="utf-8" data-replace="static-lazyload"></script>', function (file){
       assert(file.isBuffer());
       assert.equal(file.contents.toString('utf8'), htmlWrapper('<script src="https://a.alipayobjects.com/static/lazyload/2.0.3/lazyload.min.js" charset="utf-8"></script>'));
       done();
@@ -43,19 +43,27 @@ describe('gulp-h5packer', function (){
 
   });
 
-  it('#css文件嵌入', function(done) {
+  it('#css文件嵌入 with cleanCSS', function(done) {
     parseHelper('<link rel="stylesheet" href="assets/fake.css" data-replace="true" />', function (file){
       assert(file.isBuffer());
-      assert.equal(file.contents.toString('utf8'), htmlWrapper('<style>body{font-size: 14px;}</style>'));
+      assert.equal(file.contents.toString('utf8'), htmlWrapper('<style>body{font-size:14px;background-color:#123}</style>'));
       done();
     });
 
   });
 
-  it('#js文件嵌入', function(done) {
+  it('#js文件嵌入 with UglifyJS', function(done) {
     parseHelper('<script src="assets/fake.js" data-replace="true"></script>', function (file){
       assert(file.isBuffer());
-      assert.equal(file.contents.toString('utf8'), htmlWrapper('<script>console.log(\'Test\');</script>'));
+      assert.equal(file.contents.toString('utf8'), htmlWrapper('<script>!function(o){o.console.log("Test")}(window);</script>'));
+      done();
+    });
+  });
+
+  it('#velocity替换支持', function(done) {
+    parseHelper("<h1>$!title</h1>\n#if($item == 'something')\n<br>#end", function (file){
+      assert(file.isBuffer());
+      assert.equal(file.contents.toString('utf8'), htmlWrapper(null, "<h1>$!title</h1>\n#if($item == 'something')\n<br>#end"));
       done();
     });
   });
